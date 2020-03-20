cbd935f9b7e4ad1f62c2f19aa0890ecb30a9976e
Omar Sevilla <omar.sevilla.lomeli@gmail.com>
2020-03-19 21:57:13 -0600

    Proyecto nuevo creado
    Driver PWM Integrado

---
+++ ReproductorDeAudio/source/ReproductorDeAudio.c
@@ -0,0 +1,95 @@
+/*
+ * Copyright 2016-2020 NXP
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ *
+ * o Redistributions of source code must retain the above copyright notice, this list
+ *   of conditions and the following disclaimer.
+ *
+ * o Redistributions in binary form must reproduce the above copyright notice, this
+ *   list of conditions and the following disclaimer in the documentation and/or
+ *   other materials provided with the distribution.
+ *
+ * o Neither the name of NXP Semiconductor, Inc. nor the names of its
+ *   contributors may be used to endorse or promote products derived from this
+ *   software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/**
+ * @file    ReproductorDeAudio.c
+ * @brief   Application entry point.
+ */
+#include <stdio.h>
+#include "board.h"
+#include "peripherals.h"
+#include "pin_mux.h"
+#include "clock_config.h"
+#include "MKL25Z4.h"
+#include "fsl_debug_console.h"
+#include "fsl_port.h"
+#include "fsl_tpm.h"
+#include "fsl_clock.h"
+/* TODO: insert other include files here. */
+
+/* TODO: insert other definitions and declarations here. */
+
+/*
+ * @brief   Application entry point.
+ */
+int main(void) {
+
+	/* Init board hardware. */
+	BOARD_InitBootPins();
+	BOARD_InitBootClocks();
+	BOARD_InitBootPeripherals();
+	/* Init FSL debug console. */
+	BOARD_InitDebugConsole();
+
+	CLOCK_EnableClock(kCLOCK_PortB);                           /* Port B Clock Gate Control: Clock enabled */
+	PORT_SetPinMux(PORTB, 19u, kPORT_MuxAlt3);           /* PORTB19 (pin 54) is configured as TPM2_CH1 */
+
+	/* Select the clock source for the TPM counter as kCLOCK_PllFllSelClk */
+	CLOCK_SetTpmClock(1U);
+
+	tpm_config_t tpmInfo;
+	TPM_GetDefaultConfig(&tpmInfo);
+
+	/* Initialize TPM module */
+	TPM_Init(TPM2, &tpmInfo);
+
+	tpm_chnl_pwm_signal_param_t tpmParam;
+	/* Configure tpm params with frequency 24kHZ */
+	tpmParam.chnlNumber = (tpm_chnl_t)1U;
+	tpmParam.level = kTPM_LowTrue;
+	tpmParam.dutyCyclePercent = 10u;
+
+	TPM_SetupPwm(TPM2, &tpmParam, 1U, kTPM_CenterAlignedPwm, 24000U, CLOCK_GetFreq(kCLOCK_PllFllSelClk));
+
+	TPM_StartTimer(TPM2, kTPM_SystemClock);
+
+	PRINTF("Hello World\n");
+
+	/* Force the counter to be placed into memory. */
+	volatile static int i = 0 ;
+	/* Enter an infinite loop, just incrementing a counter. */
+	while(1) {
+		i++ ;
+		/* 'Dummy' NOP to allow source level single stepping of
+            tight while() loop */
+		__asm volatile ("nop");
+	}
+	return 0 ;
+}
